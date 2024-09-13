8922cc8e7c346c9100e799188ca10495d8ac78fa
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-09-13 17:58:06 +0200

    Added BearController.sc added chase and Return to homeBase functionality for the bear.

--- Capstone/Assets/Scripts/BearController.cs
+++ Capstone/Assets/Scripts/BearController.cs
@@ -1,26 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BearController : MonoBehaviour
 {
     [SerializeField] Transform homeBase;
     [SerializeField] Transform target;
 
-    [SerializeField] float homebaseStopingDistance = 2f;
+    [SerializeField] float homebaseStopDistance = 2f;
     [SerializeField] float chaseRange = 5f;
     [SerializeField] float stopingDistance = 2f;
-    [SerializeField] float runSpeed = 4f;
     [SerializeField] float healt = 15f;
 
-    Rigidbody rbody;
+    [SerializeField] bool ReturnsHome = true;
+
+    NavMeshAgent agent;
     Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rbody = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
 
     }
@@ -30,9 +32,33 @@ public class BearController : MonoBehaviour
     {
         float distanceToTarged = Vector3.Distance(transform.position, target.position);
         bool inChaseRanche = distanceToTarged <= chaseRange;
+        bool inStopDictance = distanceToTarged <= stopingDistance;
+
+        float distanceToHomeBase = Vector3.Distance(transform.position, homeBase.position);
+        bool inHomeBaseStopDistance = distanceToHomeBase <= homebaseStopDistance;
+
         if (inChaseRanche)
         {
-            
+            if (!inStopDictance) 
+            {
+                agent.isStopped = false;
+                agent.destination = target.position;
+            }
+            else if (inStopDictance) { agent.isStopped = true; }
+
+        }
+        else
+        {
+            if (ReturnsHome)
+            {
+                if (!inHomeBaseStopDistance)
+                {
+                    agent.isStopped = false;
+                    agent.destination = homeBase.position;
+                }
+                else if (inHomeBaseStopDistance) { agent.isStopped = true; }
+            }
+
         }
 
     }
